1b3a69471fcd97ff2c99b710d0c8b00b7afa317f
apblair <apblair95@gmail.com>
2023-03-30 00:03:14 -0700

    update v0.1.0

--- run-JupyterLab-Wynton-MPRAbase.sh
+++ run-JupyterLab-Wynton-MPRAbase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-singularity run --bind /scratch/MPRA-Prime:/home/jovyan/ MPRAbase.sif jupyter lab --port=9595 --no-browser --ip=0.0.0.0 --notebook-dir /home/jovyan/
+singularity run --bind /scratch/MPRA-Prime:/home/jovyan/ env/MPRAbase.sif jupyter lab --port=9595 --no-browser --ip=0.0.0.0 --notebook-dir /home/jovyan/
