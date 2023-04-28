2567e048112031d1fc8f21e555542cecdaaec462
apblair <apblair95@gmail.com>
2023-04-28 15:34:21 -0700

    Update env, demos, and analysis configs v0.1.0

--- run-JupyterLab-Wynton-MPRAbase.sh
+++ run-JupyterLab-Wynton-MPRAbase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # APB: 9595
 # JJ: 4381
-singularity run --bind /scratch/MPRA-Prime:/home/jovyan/ env/MPRAbase.sif jupyter lab --port=$1 --no-browser --ip=0.0.0.0 --notebook-dir /home/jovyan/
+singularity run --bind /scratch/MPRAhub:/home/jovyan/ env/MPRAbase.sif jupyter lab --port=$1 --no-browser --ip=0.0.0.0 --notebook-dir /home/jovyan/
