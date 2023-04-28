a803f42d498beb5f26eb6f295b6c7d61a7555c9b
apblair <apblair95@gmail.com>
2023-04-28 16:08:21 -0700

    Add cache dir for BiocFileCache library

---
+++ cache/2dbb41f836367_analysis-secondary.R
@@ -0,0 +1,8 @@
+library(iSEE)
+
+initial <- list(SampleAssayPlot(YAxisSampleName='rep1',
+                                                XAxis='Sample name',
+                                                XAxisSampleName='rep2',
+                                                Assay='log2-ActivityScore',
+                                                ColorByDefaultColor = 'red' ,
+                                                ColorBySampleNameAssay = 'log2-ActivityScore'))
