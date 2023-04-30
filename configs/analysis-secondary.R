4eaae86190bd03c2814e9f021bedaca159fbe021
apblair <apblair95@gmail.com>
2023-04-29 19:46:26 -0700

    update secondary analysis config v0.1.0

--- configs/analysis-secondary.R
+++ configs/analysis-secondary.R
@@ -1,8 +1,25 @@
 library(iSEE)
 
-initial <- list(SampleAssayPlot(YAxisSampleName='rep1',
-                                                XAxis='Sample name',
-                                                XAxisSampleName='rep2',
-                                                Assay='log2-ActivityScore',
-                                                ColorByDefaultColor = 'red' ,
-                                                ColorBySampleNameAssay = 'log2-ActivityScore'))
+initial <- list()
+
+################################################################################
+# Settings for Row data table 1
+################################################################################
+
+initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "A:HNF4A-ChMod_chr12:106514415-106514559_[chr12:106514401-106514572]", 
+    Search = "", SearchColumns = c("", "", "", ""), HiddenColumns = character(0), 
+    VersionInfo = list(iSEE = structure(list(c(2L, 10L, 0L)), class = c("package_version", 
+    "numeric_version"))), PanelId = c(RowDataTable = 1L), PanelHeight = 500L, 
+    PanelWidth = 4L, SelectionBoxOpen = FALSE, RowSelectionSource = "---", 
+    ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, 
+    ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE, 
+    ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+# replication_corr_panel <- SampleAssayPlot(YAxisSampleName='rep1',
+#                 XAxis='Sample name',
+#                 XAxisSampleName='rep2',
+#                 Assay='log2-ActivityScore',
+#                 ColorByDefaultColor = 'red' ,
+#                 ColorBySampleNameAssay = 'log2-ActivityScore')
+
+# initial <- list(replication_corr_panel, ColumnData())
