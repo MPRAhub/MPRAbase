eb8aa27b0167382d2d603c519873412f03c6ab35
apblair <apblair95@gmail.com>
2023-05-15 15:45:06 -0700

    Add cli args

--- run-local-MPRAbase.sh
+++ run-local-MPRAbase.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
-# "${PWD}":
-# docker run -it --rm -p 10000:8888 -v /Users/andrewblair/Desktop/Development:/home/jovyan/work docker.synapse.org/syn51118207/mprabase:arm64-v0.1.0
-# docker run -it --rm -p 3838:3838 -p 8787:8787 -v /Users/andrewblair/Desktop/Development:/home/jovyan/work docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1
-docker run -p 8888:8888 -p 3838:3838 -v /Users/andrewblair/Desktop/MPRA-Prime:/home/jovyan/work docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1
+
+# Command Line Arguments
+# $1 (e.g., 8888) port in the container to port $2 (e.g., 8888) on the Docker host 
+# $3 (e.g., 3838) R shiny app port in the container to port $4 (e.g., 3838) on the Docker host
+# $5 mount our local directories ("${PWD}") as a shared volume to a directory inside the container (e.g., home/jovyan.work)
+
+# Example
+# bash run-local-MPRAbase.sh 8888 8888 3838 3838 /local/dir/path
+
+docker run -p $1:$2 -p $3:$4 -v $5:/home/jovyan/work docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1
