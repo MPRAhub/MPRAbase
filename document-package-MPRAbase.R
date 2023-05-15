c9372a224d7dfdd46b23153f52f968c20e1d5020
apblair <apblair95@gmail.com>
2023-05-15 13:56:36 -0700

    rm MPRAbase build scripts

--- document-package-MPRAbase.R
+++
@@ -1,6 +0,0 @@
-library("devtools")
-library("roxygen2")
-
-# NOTE: Developed in the (JupyterLab) container docker.synapse.org/syn51118207/mprabase:v0.1.0 
-setwd("/home/jovyan/work/./MPRAbase")
-document() # NOTE: Last ran on 2.21.23 to document create-SE-object NAMESPACE
