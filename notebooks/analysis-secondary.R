2a24e70e71d332221bb9ea802a633eb5df45d49b
Andrew Blair <andrew.blair@ucsf.edu>
2023-05-15 14:15:52 -0700

    Delete analysis-secondary.R

--- notebooks/analysis-secondary.R
+++
@@ -1,8 +0,0 @@
-library(iSEE)
-
-initial <- list(SampleAssayPlot(YAxisSampleName='rep1',
-                                                XAxis='Sample name',
-                                                XAxisSampleName='rep2',
-                                                Assay='log2-ActivityScore',
-                                                ColorByDefaultColor = 'red' ,
-                                                ColorBySampleNameAssay = 'log2-ActivityScore'))
